c3c97a040b743eddec40397e679942035d432277
kysika <kysika@foxmail.com>
2021-06-26 14:44:14 +0800

    fix: an error of fill template

--- core/renderer/index.ts
+++ core/renderer/index.ts
@@ -34,28 +34,29 @@ export function createHeadTag(heads: HTMLHeadMeta[]) {
 
 export function createScriptTag(scripts: HTMLScriptMeta[]) {
 	function reducer(current: string, s: HTMLScriptMeta) {
-		return s.type === "block" ? `${current}\n<script src="${s.content}"></script>` : `${current}\n<script>${s.content}</script>`;
+		return s.type === "url" ? `${current}\n<script src="${s.content}"></script>` : `${current}\n<script>${s.content}</script>`;
 	}
 	return scripts.reduce(reducer, "");
 }
 
 const placeholder = {
-	content: "<!-- content -->",
-	scirpts: "<!-- scripts -->",
-	title: "<!-- title -->",
-	heads: "<!-- heads -->",
+	content: /<!-- content -->/g,
+	scirpts: /<!-- scripts -->/g,
+	title: /<!-- title -->/g,
+	heads: /<!-- heads -->/g,
 };
 
 // replace content use html comment
 async function render(options: RenderOption) {
 	const template = await fs.readFile(options.template, "utf-8");
-	template.replace(/<script.*type="module".*>.*<\/script>/, "");
-	template.replace(placeholder.content, options.content);
-	template.replace(placeholder.heads, createHeadTag(options.heads || []));
-	template.replace(placeholder.title, options.title);
-	template.replace(placeholder.scirpts, createScriptTag(options.scripts || []));
+	const result = template
+		.replace(/<script.*type="module".*>.*<\/script>/, "")
+		.replace(placeholder.content, options.content)
+		.replace(placeholder.heads, createHeadTag(options.heads || []))
+		.replace(placeholder.title, options.title)
+		.replace(placeholder.scirpts, createScriptTag(options.scripts || []));
 	await fs.ensureFile(options.output);
-	await fs.writeFile(options.output, template, { flag: "w+", encoding: "utf-8" });
+	await fs.writeFile(options.output, result, { flag: "w+", encoding: "utf-8" });
 }
 
 export const renderer = {
